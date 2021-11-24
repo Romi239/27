4e55747908f1e9d79dd935006d4e080e2029cb99
Romy239 <rominamaximobadillo@gmail.com>
2021-11-23 18:51:42 -0600

    hola

---
+++ b.js
@@ -0,0 +1,20 @@
+/*function AreaCircunferencia(r){
+    const pi = 3.14;
+    area = pi * (r * r);
+    return area;
+}
+
+let pepito = prompt('Ingresa el valor del radio: ');
+resultado = AreaCircunferencia(pepito)
+console.log(resultado);
+
+function AreaCuadrado(lado){
+    area = lado * lado
+    return area;
+}
+let ahh = prompt('Ingresa el lado del cuadrado : ');
+resultado = AreaCuadrado(ahh)
+console.log(resultado)*/
+
+
+
